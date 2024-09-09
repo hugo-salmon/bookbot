b9c9d0d292260742b62707b8f6c6d448eada2815
Hugo <hugo@mbp-de-hugo.home>
2024-09-10 00:48:08 +0200

    change function

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 def count_words(text):
-    words = text.split()  
-    return len(words)    
+    words = text.split()
+    return len(words)
 
 def count_characters(text):
-    char_count = {}  
-    text = text.lower()  
-    
+    char_count = {}
+    text = text.lower()
+
     for char in text:
-        if char.isalpha():  
+        if char.isalpha():
             if char in char_count:
-                char_count[char] += 1  
+                char_count[char]+=1
             else:
-                char_count[char] = 1   
+                char_count[char] = 1
 
     sorted_char_count = sorted(char_count.items(), key=lambda x: x[1], reverse=True)
-
-    return sorted_char_count  
+    
+    for char, count in sorted_char_count:
+        print(f"Le caractère '{char}' a été trouvé {count} fois")
 
 def main():
     with open('books/frankenstein.txt', 'r') as f:
         file_contents = f.read()
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words(file_contents)} mots ont été trouvés dans le document")
+    count_characters(file_contents)
+    print("--- End report ---")
 
-    word_count = count_words(file_contents)
-    print(f"Le livre 'Frankenstein' contient {word_count} mots.")
-    
-    character_count = count_characters(file_contents)
-    print("Nombre d'occurrences de chaque caractère (trié par ordre décroissant) :")
-    print(character_count)
-
-if __name__ == "__main__":
-    main()
+main()
